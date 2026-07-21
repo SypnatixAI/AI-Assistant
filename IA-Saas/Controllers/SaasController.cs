957d91881fc4c150f9bcca59530599d49cb1c18e
gtchi <giovani.tchibozo@yahoo.fr>
2026-07-20 21:38:45 -0400

    project init

---
+++ IA-Saas/Controllers/SaasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IA_Saas.Controllers
+{
+    public class SaasController: ControllerBase
+    {
+        public async Task<IActionResult> AuthenticateUser()
+        {
+
+        }
+    }
+}
