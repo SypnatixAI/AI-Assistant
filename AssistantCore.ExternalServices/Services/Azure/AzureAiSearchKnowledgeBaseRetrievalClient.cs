646f941a6820004324f9de267fc516a082148d52
gioJose-dev <giovanijose.tchibozo@gmail.com>
2026-09-07 23:10:20 -0400

    fix(search): use supported knowledge retrieval output size field

--- AssistantCore.ExternalServices/Services/Azure/AzureAiSearchKnowledgeBaseRetrievalClient.cs
+++ AssistantCore.ExternalServices/Services/Azure/AzureAiSearchKnowledgeBaseRetrievalClient.cs
@@ -105,7 +105,7 @@ public sealed class AzureAiSearchKnowledgeBaseRetrievalClient
             retrievalReasoningEffort = new { kind = "low" },
             includeActivity = true,
             maxRuntimeInSeconds = request.MaxRuntimeInSeconds,
-            maxOutputSizeInTokens = request.MaxOutputSizeInTokens,
+            maxOutputSize = request.MaxOutputSizeInTokens,
             maxOutputDocuments = request.MaximumResults,
             knowledgeSourceParams = new[]
             {
