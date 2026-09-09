8cd53809aa5952c89f9a90abbe267b0f8f4ce751
gioJose-dev <giovanijose.tchibozo@gmail.com>
2026-09-09 01:25:32 -0400

    fix(foundry): inject external client logger

--- AssistantCore.Service/Infrastructure/Foundry/FoundryAgentServiceCollectionExtensions.cs
+++ AssistantCore.Service/Infrastructure/Foundry/FoundryAgentServiceCollectionExtensions.cs
@@ -3,6 +3,7 @@ using AssistantCore.ExternalServices.Services.Foundry;
 using AssistantCore.Service.Application.Services.Messages.AgentRuntime;
 using AssistantCore.Service.Infrastructure.Foundry.Configuration;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
 
 namespace AssistantCore.Service.Infrastructure.Foundry;
 
@@ -27,7 +28,8 @@ public static class FoundryAgentServiceCollectionExtensions
                 new FoundryAgentClientSettings(
                     options.ProjectEndpoint,
                     options.AgentName,
-                    options.AgentVersion));
+                    options.AgentVersion),
+                serviceProvider.GetRequiredService<ILogger<FoundryAgentExternalClient>>());
         });
 
         services.AddSingleton<IFoundryAgentClient, FoundryAgentClientAdapter>();
