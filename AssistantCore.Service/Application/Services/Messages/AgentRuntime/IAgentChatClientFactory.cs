5f253e9d6f855467741eaeceb63fff5ca0cf9b7a
gioJose-dev <giovanijose.tchibozo@gmail.com>
2026-09-09 00:56:36 -0400

    refactor(foundry): remove obsolete IAgentChatClientFactory.cs

--- AssistantCore.Service/Application/Services/Messages/AgentRuntime/IAgentChatClientFactory.cs
+++
@@ -1,9 +0,0 @@
-using AssistantCore.Service.Application.Models.Messages.AiModels;
-using Microsoft.Extensions.AI;
-
-namespace AssistantCore.Service.Application.Services.Messages.AgentRuntime;
-
-public interface IAgentChatClientFactory
-{
-    IChatClient Create(SelectedAiModel selectedModel);
-}
