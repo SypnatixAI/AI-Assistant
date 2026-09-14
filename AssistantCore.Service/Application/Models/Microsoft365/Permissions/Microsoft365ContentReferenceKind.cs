e63bacd93ca099b7c13158f0c18270f948ec0afe
gioJose-dev <giovanijose.tchibozo@gmail.com>
2026-09-14 19:27:33 -0400

    refactor: keep Outlook ACL outside document reference model

--- AssistantCore.Service/Application/Models/Microsoft365/Permissions/Microsoft365ContentReferenceKind.cs
+++ AssistantCore.Service/Application/Models/Microsoft365/Permissions/Microsoft365ContentReferenceKind.cs
@@ -3,6 +3,5 @@ namespace AssistantCore.Service.Application.Models.Microsoft365.Permissions;
 public enum Microsoft365ContentReferenceKind
 {
     DriveItem,
-    ListItem,
-    OutlookMessage
+    ListItem
 }
