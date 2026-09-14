7d6ef19b7f4d3c2c526bc40dcdc0e2d20ad1934d
gioJose-dev <giovanijose.tchibozo@gmail.com>
2026-09-14 19:21:05 -0400

    feat: support Outlook message ACL references

--- AssistantCore.Service/Application/Models/Microsoft365/Permissions/Microsoft365ContentReferenceKind.cs
+++ AssistantCore.Service/Application/Models/Microsoft365/Permissions/Microsoft365ContentReferenceKind.cs
@@ -3,5 +3,6 @@ namespace AssistantCore.Service.Application.Models.Microsoft365.Permissions;
 public enum Microsoft365ContentReferenceKind
 {
     DriveItem,
-    ListItem
+    ListItem,
+    OutlookMessage
 }
