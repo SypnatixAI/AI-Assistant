9a8870210b8cabec9ea64089c56107b138d28ad9
gioJose-dev <giovanijose.tchibozo@gmail.com>
2026-09-13 21:04:49 -0400

    feat(usage): register quota cache and background persistence

--- AssistantCore.Service/Application/ServiceCollectionExtensions.cs
+++ AssistantCore.Service/Application/ServiceCollectionExtensions.cs
@@ -81,6 +81,10 @@ public static class ServiceCollectionExtensions
                 options => options.DefaultMonthlyTokenLimit > 0,
                 $"{UsageOptions.SectionName}:{nameof(UsageOptions.DefaultMonthlyTokenLimit)} must be greater than zero.")
             .ValidateOnStart();
+        services.AddSingleton<IUsageQuotaCache, UsageQuotaCache>();
+        services.AddSingleton<UsageConsumptionQueue>();
+        services.AddHostedService<UsageQuotaCacheInitializer>();
+        services.AddHostedService<UsagePersistenceWorker>();
         services.AddScoped<IUsageTrackingService, UsageTrackingService>();
         services.AddScoped<ISendMessageCommandValidator, SendMessageCommandValidator>();
         services.AddSingleton<IConversationCursorCodec, ConversationCursorCodec>();
