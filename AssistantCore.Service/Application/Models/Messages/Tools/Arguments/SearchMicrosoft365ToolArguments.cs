da36d14254eeaa21d6cfeb54a28ffd14491628e4
gioJose-dev <giovanijose.tchibozo@gmail.com>
2026-09-15 19:28:10 -0400

    refactor(search): remove legacy search arguments

--- AssistantCore.Service/Application/Models/Messages/Tools/Arguments/SearchMicrosoft365ToolArguments.cs
+++
@@ -1,7 +0,0 @@
-namespace AssistantCore.Service.Application.Models.Messages.Tools.Arguments;
-
-public sealed record SearchMicrosoft365ToolArguments(
-    string Query,
-    IReadOnlyCollection<string>? SourceTypes,
-    DateOnly? DateFrom,
-    DateOnly? DateTo);
