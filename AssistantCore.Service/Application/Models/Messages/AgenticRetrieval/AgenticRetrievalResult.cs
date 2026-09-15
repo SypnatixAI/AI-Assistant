c61cec00a6389369c141a48d2328aac6eefa24a9
gioJose-dev <giovanijose.tchibozo@gmail.com>
2026-09-15 19:33:54 -0400

    refactor(search): remove legacy agentic result model

--- AssistantCore.Service/Application/Models/Messages/AgenticRetrieval/AgenticRetrievalResult.cs
+++
@@ -1,6 +0,0 @@
-namespace AssistantCore.Service.Application.Models.Messages.AgenticRetrieval;
-
-public sealed record AgenticRetrievalResult(
-    string MergedContent,
-    IReadOnlyCollection<AgenticRetrievalReference> References,
-    IReadOnlyCollection<AgenticRetrievalActivity> Activity);
