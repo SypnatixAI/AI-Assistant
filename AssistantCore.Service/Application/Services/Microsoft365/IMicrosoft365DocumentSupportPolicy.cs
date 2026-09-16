cb456a20509d582b3e4aca2666bb6d2366db708b
gioJose-dev <giovanijose.tchibozo@gmail.com>
2026-09-15 20:48:18 -0400

    Remove legacy Microsoft 365 ingestion services

--- AssistantCore.Service/Application/Services/Microsoft365/IMicrosoft365DocumentSupportPolicy.cs
+++
@@ -1,6 +0,0 @@
-namespace AssistantCore.Service.Application.Services.Microsoft365;
-
-public interface IMicrosoft365DocumentSupportPolicy
-{
-    bool IsSupported(string fileName, string? mimeType);
-}
--- AssistantCore.Service/Application/Services/Microsoft365/IMicrosoft365SearchIndexInitializer.cs
+++
@@ -1,6 +0,0 @@
-namespace AssistantCore.Service.Application.Services.Microsoft365;
-
-public interface IMicrosoft365SearchIndexInitializer
-{
-    Task EnsureCreatedAsync(CancellationToken cancellationToken = default);
-}
