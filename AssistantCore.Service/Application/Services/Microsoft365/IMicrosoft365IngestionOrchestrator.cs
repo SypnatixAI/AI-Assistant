72a529a79ea25c08eb6d27c848693660ec23ff1d
gioJose-dev <giovanijose.tchibozo@gmail.com>
2026-09-15 20:42:48 -0400

    Remove legacy ingestion orchestrator contract

--- AssistantCore.Service/Application/Services/Microsoft365/IMicrosoft365IngestionOrchestrator.cs
+++
@@ -1,8 +0,0 @@
-namespace AssistantCore.Service.Application.Services.Microsoft365;
-
-public interface IMicrosoft365IngestionOrchestrator
-{
-    Task ScheduleInitialSynchronizationAsync(
-        Guid connectionId,
-        CancellationToken cancellationToken = default);
-}
